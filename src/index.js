2e91780c6c380e3b8ad73def7c5e871d05b0e209
Tomas Garcia <tomasgarciadev@gmail.com>
2022-05-13 16:24:34 +0200

    Animals image added and interated with for...in

--- src/index.js
+++ src/index.js
@@ -2,18 +2,36 @@ import { animals } from './animals'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
 const title = '';
 
 const background = <img 
   className='background'
   alt='ocean'
-  src={require('./images/ocean.jpg')}
+  src='/images/ocean.jpg'
 />
+
+let images = [];
+
+for (const animal in animals) {
+  images.push(<img 
+    key={animal}
+    className='animal'
+    alt={animal}
+    src={animals[animal].image}
+    arial-label={animal}
+    role='button'
+  />)
+}
 
 const animalFacts = (
   <div>
     <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
     { background }
+    <div className='animals'>
+    { images }
+    </div>
   </div>
 );
 
